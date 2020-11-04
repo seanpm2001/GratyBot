02bfb8e27b4cd3756ac5108dc42bca7064e8adc1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-03 19:09:14 -0800

    GratyBotMainV1.py
    An archive of the first version of the GratyBot Main thread script.

---
+++ GratyBot/OldVersions/GratyBotMainV1.py
@@ -0,0 +1,15 @@
+# Start of script
+# Library import
+import random
+# Lists
+luxuries = ["add_more_here"]
+# The_Internet (note: apparently, this is a recognized term in Python syntax. Found out before I closed the terms with parantheses and removed the capital)
+modern_utilities = ["the_Internet", "wireless_technology", "food", "drink", "add_more_here"] 
+# Start of program
+print ("GratyBot Main thread")
+print ("GratyBot is here to help you realize how much you really have and what to be grateful for")
+startGraty = input("Press [ENTER] key to start")
+print ("Program currently under construction")
+noMore = input("Press [ENTER] key to quit")
+print ("The program should now be closed. If the window is not closing, try closing it with the close button. If that doesn't work, end the process with a process/task manager")
+# End of script
